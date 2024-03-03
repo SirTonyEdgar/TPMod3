d18b815f3f8f4a7d21e8f9695d05d76fe88c20ed
SirTonyEdgar <121780770+SirTonyEdgar@users.noreply.github.com>
2024-03-03 19:02:50 +0700

    Add files via upload

---
+++ TPMod3/TPMod3/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace TPMod3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BUTTON = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BUTTON
+            // 
+            this.BUTTON.Location = new System.Drawing.Point(194, 0);
+            this.BUTTON.Name = "BUTTON";
+            this.BUTTON.Size = new System.Drawing.Size(75, 23);
+            this.BUTTON.TabIndex = 0;
+            this.BUTTON.Text = "BUTTON";
+            this.BUTTON.UseVisualStyleBackColor = true;
+            this.BUTTON.Click += new System.EventHandler(this.BUTTON_Click);
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(12, 0);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(133, 20);
+            this.input.TabIndex = 1;
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 26);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(257, 20);
+            this.output.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1251, 450);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.BUTTON);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BUTTON;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox output;
+    }
+}
+
---
+++ TPMod3/TPMod3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPMod3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BUTTON_Click(object sender, EventArgs e)
+        {
+            String nama = input.Text;
+            output.Text = "halo" + nama;
+        }
+    }
+}
